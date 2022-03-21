f90bebc13e39b4adb0a144715ef76655a595afe4
BBrandford11 <brandfordbryce@gmail.com>
2022-03-20 19:18:44 -0600

    add

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,13 +1,16 @@
 function TweetForm() {
+  const doStuff = (e) => {
+    e.preventDefault()
+  }
   return (
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
+      <form onClick={doStuff} method="post" action="/tweets" className="newtweet__form">
         <textarea
           className="form__textarea"
           name="text"
           placeholder="What are you humming about?"
         ></textarea>
-        <input type="submit" value="Tweet" className="form__input" />
+        <input onClick={doStuff} type="submit" value="Tweet" className="form__input" />
         <span className="form__counter">140</span>
       </form>
     </section>
